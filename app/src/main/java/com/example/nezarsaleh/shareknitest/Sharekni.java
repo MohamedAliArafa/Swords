75aa2187b37693ae154c5e92da5bd4225ccf92a4
Nezar Saleh <nezarsalehhasan@gmail.com>
2015-10-18 14:50:31 +0200

    18/10/2015 --- 2:50 PM ----- images

--- app/src/main/java/com/example/nezarsaleh/shareknitest/Sharekni.java
+++ app/src/main/java/com/example/nezarsaleh/shareknitest/Sharekni.java
@@ -126,15 +126,6 @@ public class Sharekni extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-            Intent intent= new Intent(getBaseContext(), WelcomePage.class);
-            startActivity(intent);
-        }
-
-
-
         return super.onOptionsItemSelected(item);
     }
 }
