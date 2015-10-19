298eaef392999766875192dc3c959156a6617baf
Nezar Saleh <nezarsalehhasan@gmail.com>
2015-10-19 13:03:48 +0200

    19/10/2015 ---  ----- 7 inch  *3

--- app/src/main/java/com/example/nezarsaleh/shareknitest/QuickSearchResults.java
+++ app/src/main/java/com/example/nezarsaleh/shareknitest/QuickSearchResults.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.IntentCompat;
@@ -40,6 +41,7 @@ public class QuickSearchResults extends AppCompatActivity {
     SharedPreferences myPrefs;
     ListView lvResult;
     private Toolbar toolbar;
+    String ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +50,7 @@ public class QuickSearchResults extends AppCompatActivity {
         lvResult = (ListView) findViewById(R.id.lv_searchResult);
 
         myPrefs = this.getSharedPreferences("myPrefs", 0);
-        String ID = myPrefs.getString("account_id", null);
+        ID = myPrefs.getString("account_id", null);
 //        Bundle in = getIntent().getExtras();
 //        Log.d("Intent Id :", String.valueOf(in.getInt("DriverID")));
 
@@ -75,63 +77,73 @@ public class QuickSearchResults extends AppCompatActivity {
         From_RegionEnName_txt.setText(From_RegionEnName);
         To_RegionEnName_txt.setText(To_RegionEnName);
 
-        char gender = 'N';
-        String Time = "";
-        int FromEmId = 2;
-        int FromRegId = 4;
-        int ToEmId = 4;
-        int ToRegId = 20;
-        int pref_lnag = 0;
-        int pref_nat = 0;
-        int Age_Ranged_id = 0;
-        String StartDate = "";
-        int saveFind = 1;
 
-        boolean exists = false;
-        try {
-            SocketAddress sockaddr = new InetSocketAddress("www.google.com", 80);
-            Socket sock = new Socket();
-            int timeoutMs = 2000;   // 2 seconds
-            sock.connect(sockaddr, timeoutMs);
-            exists = true;
-        } catch (final Exception e) {
-            e.printStackTrace();
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    new AlertDialog.Builder(QuickSearchResults.this)
-                            .setTitle("Connection Problem!")
-                            .setMessage("Make sure you have internet connection")
-                            .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Intent intentToBeNewRoot = new Intent(QuickSearchResults.this, QuickSearchResults.class);
-                                    ComponentName cn = intentToBeNewRoot.getComponent();
-                                    Intent mainIntent = IntentCompat.makeRestartActivityTask(cn);
-                                    startActivity(mainIntent);
-                                }
-                            })
-                            .setNegativeButton("Exit!", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-                                }
-                            }).setIcon(android.R.drawable.ic_dialog_alert).show();
-                    Toast.makeText(QuickSearchResults.this, "Check Internet Connection", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        if (exists) {
-            if (ID == null) {
-                GetData j = new GetData();
-                j.QuickSearchForm(0, gender, Time, From_Em_Id
-                        , From_Reg_Id, To_Em_Id, To_Reg_Id, pref_lnag, pref_nat
-                        , Age_Ranged_id, StartDate, saveFind
-                        , lvResult, this);
-            } else {
-                GetData j = new GetData();
-                j.QuickSearchForm(Integer.parseInt(ID), gender, Time, From_Em_Id
-                        , From_Reg_Id, To_Em_Id, To_Reg_Id, pref_lnag, pref_nat
-                        , Age_Ranged_id, StartDate, saveFind
-                        , lvResult, this);
+
+    }
+
+    private class backTread extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            char gender = 'N';
+            String Time = "";
+            int FromEmId = 2;
+            int FromRegId = 4;
+            int ToEmId = 4;
+            int ToRegId = 20;
+            int pref_lnag = 0;
+            int pref_nat = 0;
+            int Age_Ranged_id = 0;
+            String StartDate = "";
+            int saveFind = 1;
+
+            boolean exists = false;
+            try {
+                SocketAddress sockaddr = new InetSocketAddress("www.google.com", 80);
+                Socket sock = new Socket();
+                int timeoutMs = 2000;   // 2 seconds
+                sock.connect(sockaddr, timeoutMs);
+                exists = true;
+            } catch (final Exception e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        new AlertDialog.Builder(QuickSearchResults.this)
+                                .setTitle("Connection Problem!")
+                                .setMessage("Make sure you have internet connection")
+                                .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intentToBeNewRoot = new Intent(QuickSearchResults.this, QuickSearchResults.class);
+                                        ComponentName cn = intentToBeNewRoot.getComponent();
+                                        Intent mainIntent = IntentCompat.makeRestartActivityTask(cn);
+                                        startActivity(mainIntent);
+                                    }
+                                })
+                                .setNegativeButton("Exit!", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                }).setIcon(android.R.drawable.ic_dialog_alert).show();
+                        Toast.makeText(QuickSearchResults.this, "Check Internet Connection", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
+            if (exists) {
+                if (ID == null) {
+                    GetData j = new GetData();
+                    j.QuickSearchForm(0, gender, Time, From_Em_Id
+                            , From_Reg_Id, To_Em_Id, To_Reg_Id, pref_lnag, pref_nat
+                            , Age_Ranged_id, StartDate, saveFind
+                            , lvResult, QuickSearchResults.this);
+                } else {
+                    GetData j = new GetData();
+                    j.QuickSearchForm(Integer.parseInt(ID), gender, Time, From_Em_Id
+                            , From_Reg_Id, To_Em_Id, To_Reg_Id, pref_lnag, pref_nat
+                            , Age_Ranged_id, StartDate, saveFind
+                            , lvResult, QuickSearchResults.this);
+                }
+            }
+            return null;
         }
     }
 
