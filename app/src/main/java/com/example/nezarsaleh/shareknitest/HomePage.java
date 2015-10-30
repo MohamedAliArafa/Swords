d3553fafdda41bb81722064fd4106181ebc57d78
Mohamed Arafa <ghostarafa@gmail.com>
2015-10-30 23:51:45 +0200

    31/10/2015 8:21pm  (Route Speed)

--- app/src/main/java/com/example/nezarsaleh/shareknitest/HomePage.java
+++ app/src/main/java/com/example/nezarsaleh/shareknitest/HomePage.java
@@ -320,7 +320,7 @@ public class HomePage extends ActionBarActivity implements View.OnClickListener 
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 
 
 
