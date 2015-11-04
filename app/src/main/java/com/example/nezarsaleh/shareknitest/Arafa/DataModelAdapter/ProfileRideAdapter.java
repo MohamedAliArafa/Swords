aa6488cf55d43651a0469938736367e8f42fca21
Nezar Saleh <nezarsalehhasan@gmail.com>
2015-11-04 23:41:44 +0200

    4/11/2015 ---  ----- * 3

--- app/src/main/java/com/example/nezarsaleh/shareknitest/Arafa/DataModelAdapter/ProfileRideAdapter.java
+++ app/src/main/java/com/example/nezarsaleh/shareknitest/Arafa/DataModelAdapter/ProfileRideAdapter.java
@@ -24,9 +24,9 @@ public class ProfileRideAdapter extends ArrayAdapter<BestRouteDataModel> {
 
     public ProfileRideAdapter(Context context, int resource, BestRouteDataModel[] objects) {
         super(context, resource, objects);
-        this.context=context;
-        this.resourse=resource;
-        this.BestrouteArray =objects;
+        this.context = context;
+        this.resourse = resource;
+        this.BestrouteArray = objects;
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
@@ -38,22 +38,20 @@ public class ProfileRideAdapter extends ArrayAdapter<BestRouteDataModel> {
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = convertView;
         ViewHolder vh;
-        if (v==null)
-        {
-            v = layoutInflater.inflate(resourse,parent,false);
-            vh= new ViewHolder();
+        if (v == null) {
+            v = layoutInflater.inflate(resourse, parent, false);
+            vh = new ViewHolder();
             vh.FromEm = (TextView) v.findViewById(R.id.em_from);
             vh.ToEm = (TextView) v.findViewById(R.id.em_to);
-            vh.FromReg= (TextView) v.findViewById(R.id.reg_from);
+            vh.FromReg = (TextView) v.findViewById(R.id.reg_from);
             vh.ToReg = (TextView) v.findViewById(R.id.reg_to);
-            vh.RouteEnName= (TextView) v.findViewById(R.id.driver_profile_RouteEnName);
+            vh.RouteEnName = (TextView) v.findViewById(R.id.driver_profile_RouteEnName);
             // vh.StartFromTime= (TextView) v.findViewById(R.id.StartFromTime);
             //  vh.EndToTime_= (TextView) v.findViewById(R.id.EndToTime_);
             // vh.driver_profile_dayWeek= (TextView) v.findViewById(R.id.driver_profile_dayWeek);
 
             v.setTag(vh);
-        }else
-        {
+        } else {
             vh = (ViewHolder) v.getTag();
         }
         BestRouteDataModel bestRouteDataModel = BestrouteArray[position];
@@ -61,10 +59,10 @@ public class ProfileRideAdapter extends ArrayAdapter<BestRouteDataModel> {
         vh.ToEm.setText(bestRouteDataModel.getToEm());
         vh.FromReg.setText(bestRouteDataModel.getFromReg());
         vh.ToReg.setText(bestRouteDataModel.getToReg());
-        vh.FromEmId=bestRouteDataModel.getFromEmId();
-        vh.FromRegid=bestRouteDataModel.getFromRegid();
-        vh.ToEmId=bestRouteDataModel.getToEmId();
-        vh.ToRegId=bestRouteDataModel.getToRegId();
+        vh.FromEmId = bestRouteDataModel.getFromEmId();
+        vh.FromRegid = bestRouteDataModel.getFromRegid();
+        vh.ToEmId = bestRouteDataModel.getToEmId();
+        vh.ToRegId = bestRouteDataModel.getToRegId();
 
 
         StringBuffer res = new StringBuffer();
@@ -72,9 +70,13 @@ public class ProfileRideAdapter extends ArrayAdapter<BestRouteDataModel> {
         String[] strArr = bestRouteDataModel.getRouteName().split(" ");
         for (String str : strArr) {
             char[] stringArray = str.trim().toCharArray();
-            stringArray[0] = Character.toUpperCase(stringArray[0]);
-            str = new String(stringArray);
-            res.append(str).append(" ");
+            if (stringArray.length != 0) {
+                stringArray[0] = Character.toUpperCase(stringArray[0]);
+                str = new String(stringArray);
+                res.append(str).append(" ");
+
+            }
+
         }
         vh.RouteEnName.setText(res);
         // vh.StartFromTime.setText(bestRouteDataModel.getStartFromTime());
@@ -84,17 +86,16 @@ public class ProfileRideAdapter extends ArrayAdapter<BestRouteDataModel> {
     }
 
 
-    static class ViewHolder
-    {
+    static class ViewHolder {
         TextView FromEm;
         TextView ToEm;
         TextView FromReg;
         TextView ToReg;
-         TextView RouteEnName;
+        TextView RouteEnName;
         //   TextView StartFromTime;
         //  TextView EndToTime_;
         // TextView driver_profile_dayWeek;
-        int FromEmId,ToEmId,FromRegid,ToRegId;
+        int FromEmId, ToEmId, FromRegid, ToRegId;
 
     }
 
