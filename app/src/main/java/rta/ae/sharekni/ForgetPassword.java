1bbd488326ffd93883ad1a104562e0a4a8702e53
Nezar Saleh <nezarsalehhasan@gmail.com>
2015-11-12 21:56:29 +0200

    11-11-2015     ----* 8

--- app/src/main/java/rta/ae/sharekni/ForgetPassword.java
+++ app/src/main/java/rta/ae/sharekni/ForgetPassword.java
@@ -1,36 +1,24 @@
 package rta.ae.sharekni;
 
 import android.annotation.TargetApi;
-import android.app.AlertDialog;
-import android.content.ComponentName;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import rta.ae.sharekni.Arafa.Classes.GetData;
-
-import rta.ae.sharekni.R;
-
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
 
 
 public class ForgetPassword extends AppCompatActivity {
 
+    Activity c;
     String mobileNumber,Email;
     EditText edit_number;
     EditText edit_mail;
@@ -47,58 +35,135 @@ public class ForgetPassword extends AppCompatActivity {
         btn_submit = (Button) findViewById(R.id.btn_Forgetpass_submit);
         txt_submit = (TextView) findViewById(R.id.txt_submit);
         initToolbar();
+        c=this;
 
 
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mobileNumber =edit_number.getText().toString();
-                Email =edit_mail.getText().toString();
-
-                if (edit_mail == null||mobileNumber.length()<= 9) {
-                    Toast.makeText(getBaseContext(), R.string.check_user_pass, Toast.LENGTH_SHORT).show();
-                }else {
-                    boolean exists = false;
-                    try {
-                        SocketAddress sockaddr = new InetSocketAddress("www.google.com", 80);
-                        Socket sock = new Socket();
-                        int timeoutMs = 2000;   // 2 seconds
-                        sock.connect(sockaddr, timeoutMs);
-                        exists = true;
-                    } catch (final Exception e) {
-                        e.printStackTrace();
-                        runOnUiThread(new Runnable() {
-                            public void run() {
-                                new AlertDialog.Builder(ForgetPassword.this)
-                                        .setTitle(getString(R.string.connection_problem))
-                                        .setMessage(getString(R.string.con_problem_message))
-                                        .setPositiveButton(getString(R.string.retry), new DialogInterface.OnClickListener() {
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                finish();
-                                                startActivity(getIntent());
-                                            }
-                                        })
-                                        .setNegativeButton(getString(R.string.goBack), new DialogInterface.OnClickListener() {
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                finish();
-                                    }
-                                }).setIcon(android.R.drawable.ic_dialog_alert).show();
-                                Toast.makeText(ForgetPassword.this, getString(R.string.connection_problem), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                    if (exists) {
-                        url = url + "mobile=" + mobileNumber + "&email=" + Email;
-                        Log.d("URL Login : ", url);
-                        GetData j = new GetData();
-                        j.ForgetPasswordForm(mobileNumber, Email, getBaseContext());
-                    }
-                }
-            }
-        });
+//
+//        btn_submit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mobileNumber =edit_number.getText().toString();
+//                Email =edit_mail.getText().toString();
+//
+//                if (edit_mail == null||mobileNumber.length()<= 9) {
+//                    Toast.makeText(getBaseContext(), R.string.check_user_pass, Toast.LENGTH_SHORT).show();
+//                }else {
+//
+//                    new back().execute();
+//
+//                        url = url + "mobile=" + mobileNumber + "&email=" + Email;
+//                        Log.d("URL Login : ", url);
+//                        GetData j = new GetData();
+//                        j.ForgetPasswordForm(mobileNumber, Email, getBaseContext());
+//                    }
+//                }
+//
+//        });
 
 
     }
+
+
+    /*
+    private class back extends AsyncTask {
+
+        ProgressDialog pDialog;
+        boolean exists = false;
+        String data;
+
+        @Override
+        protected void onPreExecute() {
+            pDialog = new ProgressDialog(ForgetPassword.this);
+            pDialog.setMessage(getString(R.string.loading) + "...");
+            pDialog.show();
+        }
+
+
+        @Override
+        protected void onPostExecute(Object o) {
+
+            if (data.equals("\"1\"")) {
+                Toast.makeText(getBaseContext(), getString(R.string.verified), Toast.LENGTH_LONG).show();
+                c.finish();
+            } else if (data.equals("\"-3\"")) {
+                Toast.makeText(getBaseContext(), getString(R.string.invalid_dob), Toast.LENGTH_LONG).show();
+                Log.d("inside -3", data);
+            } else if (data.equals("\"-4\"")) {
+                Toast.makeText(getBaseContext(), getString(R.string.lic_ver_but_no_cars), Toast.LENGTH_LONG).show();
+
+            } else if (data.equals("\"-5\"") || data.equals("\"-6\"")) {
+                Toast.makeText(getBaseContext(), getString(R.string.invalid_data), Toast.LENGTH_LONG).show();
+
+            } else if (data.equals("\"0\"")) {
+                //  Toast.makeText(context, "license verified, but no cars found ", Toast.LENGTH_LONG).show();
+                Log.d("license no json", data + " Error in Connection with the DataBase Server");
+            } else if (data.equals("\"-2\"")) {
+                Toast.makeText(getBaseContext(), getString(R.string.Register_vehicle_update), Toast.LENGTH_LONG).show();
+                c.finish();
+            }
+
+
+            hidePDialog();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                SocketAddress sockaddr = new InetSocketAddress("www.google.com", 80);
+                Socket sock = new Socket();
+                int timeoutMs = 20000;   // 2 seconds
+                sock.connect(sockaddr, timeoutMs);
+                exists = true;
+            } catch (final Exception e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        new AlertDialog.Builder(ForgetPassword.this)
+                                .setTitle(getString(R.string.connection_problem))
+                                .setMessage(getString(R.string.con_problem_message))
+                                .setPositiveButton(getString(R.string.retry), new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                        startActivity(getIntent());
+                                    }
+                                })
+                                .setNegativeButton(getString(R.string.goBack), new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                }).setIcon(android.R.drawable.ic_dialog_alert).show();
+                        Toast.makeText(ForgetPassword.this, getString(R.string.connection_problem), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            if (exists) {
+
+                GetData j = new GetData();
+
+                try {
+
+                    data = j.RegisterVehicle(Driver_ID, FileNo, full_date);
+
+                } catch (JSONException e) {
+                    hidePDialog();
+                    e.printStackTrace();
+
+                }
+            }
+            return null;
+        }
+
+
+        private void hidePDialog() {
+            if (pDialog != null) {
+                pDialog.dismiss();
+                pDialog = null;
+            }
+        }
+
+    }
+
+    */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
